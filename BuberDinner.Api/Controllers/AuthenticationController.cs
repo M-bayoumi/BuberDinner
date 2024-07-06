36a96b467cd38946851a5407cccbdb897f0da4ac
esraa tareq <154009265+esraamaslooh@users.noreply.github.com>
2024-07-06 21:26:57 +0300

    Add Authentication Controller

---
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest register)
+        {
+            return Ok(register);
+        }
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest register)
+        {
+            return Ok(register);
+        }
+    }
+}
