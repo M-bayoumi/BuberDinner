26dba04118fc29ffaee7dda453daf382c259b6ae
esraa tareq <154009265+esraamaslooh@users.noreply.github.com>
2024-07-06 21:15:27 +0300

    Add project files.

---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+#region Default Services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#endregion
+
+var app = builder.Build();
+
+#region pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+#endregion
